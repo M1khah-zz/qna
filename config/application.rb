9fdb6cffe7abfce917a181dc0434f50acf3af4aa
m1khah <alexeyenko92@gmail.com>
2016-07-24 01:05:18 +0300

    specs for question controller added

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,16 @@ module Qna
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
+    config.generators do |g|
+      g.test_framework :rspec,
+                        fixtures: true,
+                        view_spec: false,
+                        helper_spec: false,
+                        routing_specs: false,
+                        request_specs: false,
+                        controller_specs: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
     # -- all .rb files in that directory are automatically loaded.
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     # Choose a test framework:
     with.test_framework :rspec
-    
+
     # Or, choose the following (which implies all of the above):
     with.library :rails
   end
@@ -21,6 +21,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryGirl::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
